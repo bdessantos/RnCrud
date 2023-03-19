869cde574b5550cab144e17cb7dac40a609a5868
Bruno Santos <bruno.santos@valltech.com.br>
2023-03-19 18:21:37 -0300

    Cria lista de usuários

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'joaosilva@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'marianaaguiar@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635451_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'juliofragoso@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'rafaelmonteiro@empmail.com',    
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635456_960_720.png'
+    },
+];
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
+import users from "../data/users";
 
 export default props => {
-    return(
-        <Text>UserList</Text>
+
+    function getUserItem({ item: user }) {
+
+        return (
+
+            <ListItem key={user.id} bottomDivider onPress={() => props.navigation.navigate('UserForm', user)}>
+                <Avatar source={{ uri: user.avatarUrl }} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
