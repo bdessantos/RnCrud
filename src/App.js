414a302ca19bd8439a2ff47cdf070e5053e74d5d
Bruno Santos <bruno.santos@valltech.com.br>
2023-03-18 22:03:11 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import UserList from "./views/UserList";
+import UserForm from "./views/UserForm";
+import { Button, Icon } from "react-native-elements";
+
+const stack = createNativeStackNavigator()
+
+export default props => {
+    return (
+        <NavigationContainer>
+            <stack.Navigator initialRouteName="UserList" screenOptions={screenOptions}>
+                <stack.Screen
+                    name="UserList"
+                    component={UserList}
+                    options={({navigation}) => {
+                        return {
+                            title: "Lista de Usuários",
+                            headerRight: () => (
+                                <Button 
+                                onPress={() => navigation.navigate("UserForm")}
+                                    type="clear"
+                                    icon={<Icon name="add" size={25} color="white"/>}
+                                />
+                            )
+                        }
+                    }}
+                />
+                <stack.Screen
+                    name="UserForm"
+                    component={UserForm}
+                    options={{
+                        title: "Formulário de Usuários"
+                    }}
+                />
+            </stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
+}
